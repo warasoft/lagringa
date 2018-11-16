e3cfefef56012369f3c864db334d313eb1353e77
warasoft <45013513+warasoft@users.noreply.github.com>
2018-11-16 13:24:24 -0300

    Update envio.php

--- envio.php
+++ envio.php
@@ -1,9 +1,9 @@
 <?php
-	$destino = "jfernandoromano@gmail.com";
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $telefono = $_POST['telefono'];
-    $consulta = $_POST['consulta'];
+    $destino = "jfernandoromano@gmail.com";
+    $name = $_POST["name"];    
+    $email = $_POST["email"];
+    $telefono = $_POST["telefono"];
+    $consulta = $_POST["consulta"];
     $contenido = "Nombre: " . $name . "/Mi correo: " . $email . "/Mi telefono: " . $telefono . "/Mi consulta: "  $consulta;
     mail ($destino, "Contacto ", $contenido);
 ?>
