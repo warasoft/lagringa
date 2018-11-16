1c830b8dc38d1c408684806e685f8b18e39bc31c
warasoft <45013513+warasoft@users.noreply.github.com>
2018-11-16 12:32:30 -0300

    Add files via upload

---
+++ envio.php
@@ -0,0 +1,11 @@
+<?php
+	$destino = "jfernandoromano@gmail.com";
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $telefono = $_POST['telefono'];
+    $consulta = $_POST['consulta'];
+    $contenido = "Nombre: " . $name . "/Mi correo: " . $email . "/Mi telefono: " . $telefono . "/Mi consulta: "  $consulta;
+    mail ($destino, "Contacto ", $contenido);
+    header("Location: index.html");
+
+?>
