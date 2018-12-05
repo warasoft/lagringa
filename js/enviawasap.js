519958a5b2c36e6a830814243898fbe1eebfa243
warasoft <45013513+warasoft@users.noreply.github.com>
2018-12-05 14:34:08 -0300

    Update enviawasap.js

--- js/enviawasap.js
+++ js/enviawasap.js
@@ -3,12 +3,13 @@ function copiarDatos(){
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var telefono = document.getElementById("telefono").value;
+  var consulta = document.getElementById("consulta").value;
 
-  var texto = "Nombre: "+ name + " // Email: " + email + " // Telefono: " + telefono;
+  var texto = "Nombre: "+ name + " // Email: " + email + " // Telefono: " + telefono + " // Consulta: " + consulta;
 
   document.getElementById("textToEncode").innerHTML = texto;
   
-  var url = "https://api.whatsapp.com/send?phone=5493875749355&text="+encodeURIComponent(texto)
+  var url = "https://api.whatsapp.com/send?phone=5491137710839&text="+encodeURIComponent(texto)
 						
   window.open(url);
 }
